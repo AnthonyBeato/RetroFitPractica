dd40bb78ed18673f7111e8be2341cbb3bfdf4578
axba0001 <axba0001@ce.pucmm.edu.do>
2023-09-27 20:15:42 -0400

    implementacion de retrofit

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.retrofit;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.retrofit.api.APIClient;
+import com.example.retrofit.api.APIInterface;
+import com.example.retrofit.databinding.ActivityMainBinding;
+import com.example.retrofit.dto.UserSingle;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        APIInterface api = APIClient.getClient().create(APIInterface.class);
+
+        api.find(2).enqueue(new Callback<UserSingle>() {
+            @Override
+            public void onResponse(Call<UserSingle> call, Response<UserSingle> response) {
+                Log.w("onResponse", response.body().data.toString());
+            }
+
+            @Override
+            public void onFailure(Call<UserSingle> call, Throwable t) {
+                Log.w("onFailure", t.getLocalizedMessage());
+                call.cancel();
+            }
+        });
     }
 }
