0433b8b1e4c0a6ac76673a292ec8513ba70735b6
axba0001 <axba0001@ce.pucmm.edu.do>
2023-09-27 23:13:14 -0400

    creación de nueva actividad

---
+++ app/src/main/java/com/example/retrofit/DisplayUserActivity.java
@@ -0,0 +1,13 @@
+package com.example.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_user);
+    }
+}
